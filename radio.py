cc1d4f173d5a0149e8c6057fac9e32d4360764da
Sara Bee <sarabobo@gmail.com>
2020-04-14 18:24:15 +0000

    grab the current track and extract metadata

---
+++ radio.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+base_uri = 'https://api.prod.nypr.digital/api/v1'
+now_playing_uri = '/whats_on/wqxr/3/'
+
+class Radio:
+    def fetch_track():
+        uri = base_uri + now_playing_uri
+        response = requests.get(uri)
+        json = response.json()
+
+        track = json['current_playlist_item']['catalog_entry']
+        return track
+
+
+    def get_title(track):
+        return track['title']
+
+    def get_composer(track):
+        return track['composer']['name']
+
+    def get_conductor(track):
+        return track['conductor']['name']
+
+track = Radio.fetch_track()
+composer = Radio.get_composer(track)
+title = Radio.get_title(track)
+print(composer + ": " + title) 
