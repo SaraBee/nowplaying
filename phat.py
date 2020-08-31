8fa018224119b6c52c63e9de7d76b747f65fd89b
Sara Bee <sarabeedev@gmail.com>
2020-08-31 20:46:03 +0000

    use cache to avoid constant screen redraws

--- phat.py
+++ phat.py
@@ -2,6 +2,8 @@ from inky import InkyPHAT
 from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
 from radio import Radio
+import sys
+import os
 
 def formatLine(msg):
     global lines
@@ -22,27 +24,51 @@ def formatLine(msg):
                 cur_line = tok + ' '
         lines.append(cur_line)
 
-inky_display = InkyPHAT("yellow")
+inky_display = InkyPHAT('black')
 inky_display.set_border(inky_display.WHITE)
 
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
 
-font = ImageFont.truetype(FredokaOne, 18)
+font = ImageFont.truetype(FredokaOne, 20)
 
 lines = []
 
 track = Radio.fetchTrack()
-if track:
-    formatLine(Radio.getComposer(track))
-    formatLine(Radio.getTitle(track))
+if not track:
+    sys.exit()
 
+current_dir = os.path.dirname(__file__)
+cache_file = os.path.join(current_dir, 'cache')
+
+cache = open(cache_file, 'rt')
+last_track = cache.readline()
+cache.close()
+
+composer = Radio.getComposer(track)
+title = Radio.getTitle(track)
+
+if last_track == composer + ' ' + title:
+    print("skipping " + composer + ' ' + title)
+    # No need to update the display, it's already up to date
+    sys.exit()
+
+# We open the cache file a second time in w mode to overwrite
+cache = open(cache_file, 'w')
+cache.write(composer + ' ' + title)
+cache.close()
+
+formatLine(Radio.getComposer(track))
+formatLine(Radio.getTitle(track))
+
+height_counter = 0
 for i in range (0, len(lines)):
     msg = lines[i]
     w, h = font.getsize(msg)
     x = 0
-    y = i * h
+    y = height_counter
     draw.text((x, y), msg, inky_display.BLACK, font)
+    height_counter += h
 
 inky_display.set_image(img)
 inky_display.show()
